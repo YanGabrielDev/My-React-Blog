dd82dede407c7487adabc63ffd0407197aea25ac
Yan <yangabriel2012@gmail.com>
2022-10-31 08:22:54 -0300

    implementaçao do component content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import StyledEngine from "@mui/styled-engine"
+import { ReactNode } from "react"
+import * as Styled from './styles'
+export interface ContentInterface{
+    children: ReactNode,
+}
+export const Content = ({children}: ContentInterface) => {
+    return(
+        <Styled.Container>
+            {children}
+        </Styled.Container>
+    )
+}
