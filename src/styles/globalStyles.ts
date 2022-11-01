45f696c9ea9d2be6f886db7dc071ce69654bb3a6
Yan <yangabriel2012@gmail.com>
2022-11-01 09:00:51 -0300

    alteções no globalstyles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,10 +6,16 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
     padding: 0;
+
+    
     
 }
 
 body{
     color: black;
+    /* background: black; */
 }
+
+
+
 `
