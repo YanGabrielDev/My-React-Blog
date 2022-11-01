95f8f290adcd553095e5b404d325e1c057a8d9f9
Yan <yangabriel2012@gmail.com>
2022-11-01 09:00:14 -0300

    atualizações no app

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,7 +3,17 @@ import styled, {css} from "styled-components";
 export const Container = styled.div`
 ${({theme}) => css`
 width: 100%;
-height: 100%;
+max-width: 760px;
 background-color: red;
+margin:0 auto;
+height: 100%;
+display: flex;
+justify-content: center;
+flex-direction: column;
+text-align: center;
+
+
 `}
+
+
 `
