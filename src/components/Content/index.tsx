e949279af7415bf71d9e6bd9102721bed9450b0d
Yan <yangabriel2012@gmail.com>
2022-11-02 21:29:53 -0300

    fix: ajustes no component de conteudo

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,7 +2,7 @@ import StyledEngine from "@mui/styled-engine"
 import { ReactNode } from "react"
 import * as Styled from './styles'
 export interface ContentInterface{
-    children: ReactNode,
+    children?: ReactNode,
 }
 export const Content = ({children}: ContentInterface) => {
     return(
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,7 +1,8 @@
 import styled, {css} from "styled-components";
+import {ThemeInterface} from '../../styles/theme'
 
 export const Container = styled.div`
-${({theme}) => css`
+${({theme, font}: ThemeInterface) => css`
 width: 100%;
 max-width: 760px;
 background-color: red;
@@ -11,8 +12,8 @@ display: flex;
 justify-content: center;
 flex-direction: column;
 text-align: center;
-
-
+padding-top: 6rem;
+font-family: ${font?.family?.default};
 `}
 
 
