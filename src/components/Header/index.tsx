87b00124ca92d05324e36bcdba5bcba55de7c546
Yan <yangabriel2012@gmail.com>
2022-11-02 21:30:07 -0300

    fix: ajustes no component de Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import * as Styled from './styles'
 export const Header = () => {
     return (
         <Styled.Container>
-            
+            <Styled.LogoText>Blog</Styled.LogoText>
         </Styled.Container>
 
     )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,18 @@
 import styled, {css} from "styled-components";
+import {ThemeInterface} from '../../styles/theme'
 
-export const Container = styled.header`
-${({theme}) =>css`
+export const Container =  styled.header`
+${({theme}:ThemeInterface) =>css`
 width: 100%;
 height: 50px;
-background-color: ${theme.colors.black};
+background-color: ${theme?.colors?.black};
 position: fixed;
+`}
+`
+export const LogoText =  styled.h2`
+${({theme, font}:ThemeInterface) =>css`
+font-size: ${font?.sizes?.medium};
+color: white;
+
 `}
 `
