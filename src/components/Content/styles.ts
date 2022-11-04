02837f71543c5a85e119cb384bd1885f08cda817
Yan <yangabriel2012@gmail.com>
2022-11-04 02:22:37 -0300

    fix: alteração da font-family e posicionamento do texto

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -2,18 +2,17 @@ import styled, {css} from "styled-components";
 import {ThemeInterface} from '../../styles/theme'
 
 export const Container = styled.div`
-${({theme, font}: ThemeInterface) => css`
+${({theme}: ThemeInterface) => css`
 width: 100%;
 max-width: 760px;
-background-color: red;
+/* background-color: red; */
 margin:0 auto;
 height: 100%;
 display: flex;
 justify-content: center;
 flex-direction: column;
-text-align: center;
 padding-top: 6rem;
-font-family: ${font?.family?.default};
+font-family: ${theme.fonts?.family?.default};
 `}
 
 
