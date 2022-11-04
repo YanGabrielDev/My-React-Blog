bf0b39578e739f8b89d80a24e104a93800529f91
Yan <yangabriel2012@gmail.com>
2022-11-04 02:24:21 -0300

    feat: implementação da rota Posts

---
+++ src/pages/Posts/index.tsx
@@ -0,0 +1,12 @@
+export const Posts = () => {
+    return(
+        <>
+        
+        <h1>Meu primeiro Post</h1>
+        <h1>Meu primeiro Post</h1>
+        <h1>Meu primeiro Post</h1>
+        <h1>Meu primeiro Post</h1>
+        <h1>Meu primeiro Post</h1>
+        </>
+    )
+}
