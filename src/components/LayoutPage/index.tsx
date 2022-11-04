54bb23ee89020bb353c98290da8679c7bde90abb
Yan <yangabriel2012@gmail.com>
2022-11-04 02:23:38 -0300

    fix: alterações no comportamento da sidebar no mobile

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -1,17 +1,17 @@
-import {Content, ContentInterface} from '../Content/index'
-import { Header } from '../Header'
-import { SideBar } from '../SideBar'
- const LayouPage = ({children}:ContentInterface) => {
-     return(
+import { Content, ContentInterface } from "../Content/index"
+import { Header } from "../Header"
+import { SideBar } from "../SideBar"
+import { useMediaQuery } from "@mui/material"
+const LayouPage = ({ children }: ContentInterface) => {
+  const matches = useMediaQuery("(min-width:860px)")
 
+  return (
     <div>
-    <Header/>
-    <SideBar/>
-    <Content>
-     {children}
-    </Content>
-  </div>
-     )
+      <Header />
+      {matches && <SideBar />}
+      {children}
+    </div>
+  )
 }
 
-export default LayouPage;
+export default LayouPage
