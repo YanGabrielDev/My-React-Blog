6d43a45c314bbfbb788a349a9964d87db555ae0f
Yan <yangabriel2012@gmail.com>
2022-11-04 02:21:32 -0300

    feat: implementação do react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {GlobalStyles} from './styles/globalStyles'
-import { ThemeProvider } from 'styled-components';
-import {Theme} from "./styles/theme"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./routes/App"
+import { GlobalStyles } from "./styles/globalStyles"
+import { ThemeProvider } from "styled-components"
+import { theme } from "./styles/theme"
+import { BrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-    <App />
-    <GlobalStyles/>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>
-);
-
-
+)
