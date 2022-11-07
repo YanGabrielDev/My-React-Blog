9c2abd1f1ecba956596f90676b03d316194b99dd
Yan <yangabriel2012@gmail.com>
2022-11-07 09:30:52 -0300

    feat: adição da descrição do projeto na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,22 @@
+import { Link } from "react-router-dom"
 import { Content } from "../../components/Content"
 import * as Styled from "./styles"
 export const Home = () => {
   return (
     <>
       <Content>
-        <Styled.Title>Sobre o Projeto</Styled.Title>
+        <Styled.Title>Olá</Styled.Title>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-          dicta aspernatur animi ipsa explicabo ab neque adipisci assumenda
-          voluptate provident minima repellendus reprehenderit hic dolorum
-          voluptas unde vitae autem modi.
+          Meu nome é Yan Gabriel, sou estudante de Ciências da Computação e
+          desenvolvedor FrontEnd. Esse projeto foi desenvolvido com o proposito
+          de explorar alguns conhecimentos meus adquiridos com estudos e
+          experiências profissionais na área de desenvolvimento web.
+        </p>
+        <p>
+          Essa página tem uma tela dedicada a <Link to={"./posts"}>posts</Link>,
+          vivencias e experiência que eu tive em relação a jornada como
+          desenvolvedor ou algum pensamento reflexivel que eu possa querer
+          guardar
         </p>
       </Content>
     </>
