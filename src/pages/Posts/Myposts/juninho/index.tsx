bb3ab9f7a9ae2b4473b06dad0ebded241ebbd45d
Yan <yangabriel2012@gmail.com>
2022-11-08 09:10:37 -0300

    feat: implementação do post estagiário saga

---
+++ src/pages/Posts/Myposts/juninho/index.tsx
@@ -0,0 +1,38 @@
+import { Content } from "../../../../components/Content"
+import * as Styled from "../../styles"
+export const Juninho = () => {
+  return (
+    <Content>
+      <Styled.Title>A saga do estagiário</Styled.Title>
+      <Styled.Paragraph>
+        Quando comecei a estudar para seguir a carreira de desenvolvedor não
+        imaginava os desafios que estavam por vir, tanto profissionais como
+        psicológico.
+      </Styled.Paragraph>
+      <Styled.Paragraph>
+        Após um amigo me oferecer a oportunidade de ser estagiário na área de
+        desenvolvimento de software logo aceitei, nunca avia tido uma
+        experiência profissional nessa área, meu conhecimento se baseava em
+        cursos feitos em plataformas de estudo, amigos que atuavam ou estudavam
+        a área e canais do Youtube. Logo nas minhas primeiras tarefas eu me dei
+        conta de que esse conhecimento não era nem um pouco suficiente para
+        conseguir resolver as tarefas direcionadas a mim.
+      </Styled.Paragraph>
+      <Styled.Paragraph>
+        Enquanto o tempo se passava eu pegava tarefas e não conseguia
+        entregá-las sozinho, mesmo estudando a linguagem de programação que eu
+        utilizava, na maioria das vezes o meu mentor e supervisor me ajudava a
+        terminar e tentava me ensinar alguma coisa com aquilo, eu tinha que
+        lidar com a frustração de não conseguir fazer algo e o constante
+        pensamento de eu talvez eu nunca conseguiria ou de que eu era uma farsa.
+      </Styled.Paragraph>
+      <Styled.Paragraph>
+        A melhor coisa que fiz para me sentir melhor com essa frustração foi
+        parar e pensar que naquele momento eu sabia de coisas que a uma semana
+        atrás eu não sabia, que a cada conhecimento novo é um degrau a mais que
+        estou percorrendo pro tão sonhado júnior.
+      </Styled.Paragraph>
+      <Styled.Date>08/11/2022 09:03 AM</Styled.Date>
+    </Content>
+  )
+}
