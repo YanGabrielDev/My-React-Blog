5125d1a36b22a4b81a26bea7fa471fbf19014ad2
Yan <yangabriel2012@gmail.com>
2022-11-09 22:17:46 -0300

    feat: implementação da rota photos

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,6 +3,7 @@ import { Posts } from "../pages/Posts"
 import LayouPage from "../components/LayoutPage"
 import { Home } from "../pages/Home"
 import { Juninho } from "../pages/Posts/Myposts/juninho"
+import { Photos } from "../pages/Photos/index"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/Juninho" element={<Juninho />} />
           <Route path="/" element={<Home />} />
           <Route path="/Posts" element={<Posts />} />
+          <Route path="/Photos" element={<Photos />} />
         </Routes>
       </LayouPage>
     </div>
