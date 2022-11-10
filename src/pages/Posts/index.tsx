cc946fcc86115de89da27e3b35c1ca10f231120e
Yan <yangabriel2012@gmail.com>
2022-11-09 22:12:26 -0300

    fix: alterando tag p para component estilizado

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -4,10 +4,10 @@ export const Posts = () => {
   return (
     <Content>
       <Styled.TitleLink to={"/Juninho"}>A saga do estagiário</Styled.TitleLink>
-      <p>
+      <Styled.Paragraph>
         A visão e os pensamentos de um estagiário em busca da tão sonhada
         efetivação.
-      </p>
+      </Styled.Paragraph>
       <Styled.Date>08/11/2022 09:03 AM</Styled.Date>
     </Content>
   )
