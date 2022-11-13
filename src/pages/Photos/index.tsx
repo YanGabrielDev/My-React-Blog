5d540812efa2ae11e32376043e8c83c320977171
Yan <yangabriel2012@gmail.com>
2022-11-13 01:23:49 -0300

    feat: aplicando component de modal na pagina photos

--- src/pages/Photos/index.tsx
+++ src/pages/Photos/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 import { Content } from "../../components/Content"
-
+import { Thumbnail, ModalContent } from "../../components/ModalImage"
+// import Image from "../../../public/disco.jpg"
 export const Photos = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  // const showModal = () => setIsOpen((prev) => !prev)
+
   return (
     <Content>
-      <p>grid</p>
+      <Thumbnail onOpen={() => setIsOpen(true)}>
+        <img src="//placekitten.com/400/300" />
+      </Thumbnail>
+      {isOpen && (
+        <ModalContent onClose={() => setIsOpen(false)}>
+          <img src="//placekitten.com/4000/3000" />
+        </ModalContent>
+      )}
     </Content>
   )
 }
