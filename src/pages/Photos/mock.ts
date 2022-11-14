5362756c9d201be180b5477f69c52a283b0451bc
Yan <yangabriel2012@gmail.com>
2022-11-14 01:12:38 -0300

    feat: implementando mockimage

---
+++ src/pages/Photos/mock.ts
@@ -0,0 +1,185 @@
+export const image = {
+  id: 1,
+  image1: "../../assets/image/image1.jpg",
+
+  //   {
+  //     id: 21,
+  //     image: "../../assets/image/image21",
+  //   },
+  //   {
+  //     id: 22,
+  //     image: "../../assets/image/image22",
+  //   },
+  //   {
+  //     id: 23,
+  //     image: "../../assets/image/image23",
+  //   },
+  //   {
+  //     id: 24,
+  //     image: "../../assets/image/image24",
+  //   },
+  //   {
+  //     id: 25,
+  //     image: "../../assets/image/image25",
+  //   },
+  //   {
+  //     id: 26,
+  //     image: "../../assets/image/image26",
+  //   },
+  //   {
+  //     id: 27,
+  //     image: "../../assets/image/image27",
+  //   },
+  //   {
+  //     id: 28,
+  //     image: "../../assets/image/image28",
+  //   },
+  //   {
+  //     id: 29,
+  //     image: "../../assets/image/image29",
+  //   },
+  //   {
+  //     id: 30,
+  //     image: "../../assets/image/image30",
+  //   },
+  //   {
+  //     id: 31,
+  //     image: "../../assets/image/image31",
+  //   },
+  //   {
+  //     id: 32,
+  //     image: "../../assets/image/image32",
+  //   },
+  //   {
+  //     id: 33,
+  //     image: "../../assets/image/image33",
+  //   },
+  //   {
+  //     id: 34,
+  //     image: "../../assets/image/image34",
+  //   },
+  //   {
+  //     id: 35,
+  //     image: "../../assets/image/image35",
+  //   },
+  //   {
+  //     id: 36,
+  //     image: "../../assets/image/image36",
+  //   },
+  //   {
+  //     id: 37,
+  //     image: "../../assets/image/image37",
+  //   },
+  //   {
+  //     id: 38,
+  //     image: "../../assets/image/image38",
+  //   },
+  //   {
+  //     id: 39,
+  //     image: "../../assets/image/image39",
+  //   },
+  //   {
+  //     id: 40,
+  //     image: "../../assets/image/image41",
+  //   },
+  //   {
+  //     id: 42,
+  //     image: "../../assets/image/image42",
+  //   },
+  //   {
+  //     id: 43,
+  //     image: "../../assets/image/image43",
+  //   },
+  //   {
+  //     id: 44,
+  //     image: "../../assets/image/image44",
+  //   },
+  //   {
+  //     id: 45,
+  //     image: "../../assets/image/image45",
+  //   },
+  //   {
+  //     id: 46,
+  //     image: "../../assets/image/image46",
+  //   },
+  //   {
+  //     id: 47,
+  //     image: "../../assets/image/image47",
+  //   },
+  //   {
+  //     id: 48,
+  //     image: "../../assets/image/image48",
+  //   },
+  //   {
+  //     id: 49,
+  //     image: "../../assets/image/image49",
+  //   },
+  //   {
+  //     id: 50,
+  //     image: "../../assets/image/image50",
+  //   },
+  //   {
+  //     id: 51,
+  //     image: "../../assets/image/image51",
+  //   },
+  //   {
+  //     id: 52,
+  //     image: "../../assets/image/image52",
+  //   },
+  //   {
+  //     id: 53,
+  //     image: "../../assets/image/image53",
+  //   },
+  //   {
+  //     id: 54,
+  //     image: "../../assets/image/image54",
+  //   },
+  //   {
+  //     id: 55,
+  //     image: "../../assets/image/image55",
+  //   },
+  //   {
+  //     id: 56,
+  //     image: "../../assets/image/image56",
+  //   },
+  //   {
+  //     id: 57,
+  //     image: "../../assets/image/image1",
+  //   },
+  //   {
+  //     id: 58,
+  //     image: "../../assets/image/image58",
+  //   },
+  //   {
+  //     id: 59,
+  //     image: "../../assets/image/image59",
+  //   },
+  //   {
+  //     id: 60,
+  //     image: "../../assets/image/image60",
+  //   },
+  //   {
+  //     id: 61,
+  //     image: "../../assets/image/image61",
+  //   },
+  //   {
+  //     id: 62,
+  //     image: "../../assets/image/image62",
+  //   },
+  //   {
+  //     id: 63,
+  //     image: "../../assets/image/image63",
+  //   },
+  //   {
+  //     id: 64,
+  //     image: "../../assets/image/image64",
+  //   },
+  //   {
+  //     id: 65,
+  //     image: "../../assets/image/image65",
+  //   },
+  //   {
+  //     id: 66,
+  //     image: "../../assets/image/image66",
+  //   },
+}
