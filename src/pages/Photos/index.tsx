63c7651c12979b6ae5e8f18014a3228d716c188c
Yan <yangabriel2012@gmail.com>
2022-11-14 01:12:19 -0300

    fix: implementando requisição para api de fotos

--- src/pages/Photos/index.tsx
+++ src/pages/Photos/index.tsx
@@ -1,15 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Content } from "../../components/Content"
 import { Thumbnail, ModalContent } from "../../components/ModalImage"
-// import Image from "../../../public/disco.jpg"
+import * as Styled from "./styles"
+import { image } from "./mock"
+import api from "../../services/PhotoApi/index"
+import { AxiosResponse } from "axios"
 export const Photos = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-  // const showModal = () => setIsOpen((prev) => !prev)
+  const [images, setImages] = useState<any>()
 
+  const getImages = async (): Promise<any> => {
+    const res: AxiosResponse<any> = await api({})
+
+    setImages(res.data)
+    return res
+  }
+
+  useEffect(() => {
+    getImages()
+  }, [])
   return (
     <Content>
       <Thumbnail onOpen={() => setIsOpen(true)}>
-        <img src="//placekitten.com/400/300" />
+        {images.map((img: any) => {
+          return <Styled.thumbnailImage src={img.download_url} />
+        })}
       </Thumbnail>
       {isOpen && (
         <ModalContent onClose={() => setIsOpen(false)}>
