98d60bc5d5d6ee96e64af1dcf6386afae0e238e7
Yan <yangabriel2012@gmail.com>
2022-11-16 02:33:17 -0300

    fix: correçoes nas configs

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,11 @@
 declare module "*.jpg" {
     export default "" as string;
   }
+  
   declare module "*.png" {
+    export default "" as string;
+  }
+
+  declare module "*.jfif" {
     export default "" as string;
   }
