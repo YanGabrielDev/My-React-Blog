a7d6f440710761876b748e590d1ed2789bcd4c2e
Yan <yangabriel2012@gmail.com>
2022-11-16 02:35:31 -0300

    feat: implementação do profile no projeto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom"
 import { Content } from "../../components/Content"
 import * as Styled from "./styles"
+import Profile from '../../assets/Yan.jfif'
 export const Home = () => {
   return (
     <>
       <Content>
-        <Styled.Title>Olá</Styled.Title>
+        <Styled.ProfileContent>
+          <Styled.ProfilePhoto src={Profile}/>
+          <div>
+          <Styled.Title>Yan Gabriel</Styled.Title>
+          <Styled.Discription>Front End developer</Styled.Discription>
+          </div>
+        </Styled.ProfileContent>
+        <hr/>
         <Styled.Paragraph>
-          Meu nome é Yan Gabriel, sou estudante de Ciências da Computação e
-          desenvolvedor FrontEnd. Esse projeto foi desenvolvido com o proposito
+          Olá meu nome é Yan, sou estudante de Ciências da Computação e
+          apaixonado por desenvolvimento de software. Esse projeto foi desenvolvido com o proposito
           de explorar alguns conhecimentos meus adquiridos com estudos e
           experiências profissionais na área de desenvolvimento web.
         </Styled.Paragraph>
@@ -16,7 +24,7 @@ export const Home = () => {
           Essa página tem uma tela dedicada a <Link to={"./posts"}>posts</Link>,
           vivencias e experiência que eu tive em relação a jornada como
           desenvolvedor ou algum pensamento reflexivel que eu possa querer
-          guardar
+          guardar.
         </Styled.Paragraph>
       </Content>
     </>
